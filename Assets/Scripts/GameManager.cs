53c7fcb38f618f59325fdddebb83158b7134013d
Fox <jpcruzeiro2010@gmail.com>
2026-09-01 14:24:59 -0300

    Dialogos funcionando e caixa de texto sem sprite

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,19 +7,21 @@ public class GameManager : MonoBehaviour
 {
     private Player player;
     public TextMeshProUGUI text;
-    public Dialogos dialogos;
+    public GameObject CaixaDeTexto;
+    public Dialogos dialogoNPC3;
+    Dialogos dialogos;
 
-    private void Start()
+    public void Start()
     {
         player = FindAnyObjectByType<Player>();
         text.gameObject.SetActive(false);
-        dialogos = FindAnyObjectByType<Dialogos>();
+        CaixaDeTexto.SetActive(false);
     }
 
-    private void Update()
+    public void Update()
     {
         
-    if (dialogos.playerInRangeNpc3)
+    if (dialogoNPC3.playerInRangeNpc3)
     {
         Debug.Log("Player is in range of NPC3");
         if (Input.GetKey(KeyCode.C))
@@ -27,11 +29,13 @@ public class GameManager : MonoBehaviour
             player.transform.position = new Vector3(36, 17, 0);
             player.speed = 5f;
             text.gameObject.SetActive(false);
+            CaixaDeTexto.SetActive(false);
         }
     }
     if (Input.GetKey(KeyCode.Q))
         {
             text.gameObject.SetActive(false);
+            CaixaDeTexto.SetActive(false);
             player.speed = 5f;
         }
 
@@ -40,6 +44,7 @@ public class GameManager : MonoBehaviour
     {
         player.speed = 0f;
         text.gameObject.SetActive(true);
+        CaixaDeTexto.SetActive(true);
         text.text = texto;
     }
 }
